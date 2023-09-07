18fad394bb905c126fc96dbf3f51807359e3ff62
Nakamotto <343316184@qq.com>
2023-09-07 19:49:58 +0800

    update

--- autobuild.sh
+++ autobuild.sh
@@ -17,13 +17,13 @@ cd `pwd`/build &&
 cd ..
 
 #安装头文件和库文件
-if[! -d /usr/include/mymuduo]; then
+if [ ! -d /usr/include/mymuduo ]; then
     mkdir /usr/include/mymuduo
 fi
 
 for headr in 'ls *.h'
 do
-    cp $header /user/include/myduo
+    cp $header /user/include/mymuduo
 done
 
 cd `pwd` /lib/libmymuduo.so /usr/lib
