2b824f465228d7826dc4f831bdd6f8589bac5a8c
chaoxu <xuchao_202109@163.com>
2023-10-17 00:13:34 +0800

    example updated

--- example/testserver.cc
+++ example/testserver.cc
@@ -6,7 +6,7 @@ class EchoServer
 {
 
 public:
-    EchoServer(EventLoop *loop, const InetAddress &addr, const std::string &name) : loop_(loop), server_(loop, addr, name)
+    EchoServer(const InetAddress &addr, const std::string &name) : loop_(), server_(&loop_, addr, name)
     {
         server_.setConnectionCallback(std::bind(&EchoServer::onConnectionCallback, this, std::placeholders::_1));
         server_.setMessageCallback(std::bind(&EchoServer::onMessageCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
@@ -16,6 +16,8 @@ public:
     {
         server_.start();
         std::cout << "EchoServer is running......" << std::endl;
+        // (epoll_wait循环)启动mainLoop底层的Poller
+        loop_.loop();
     }
 
 private:
@@ -35,18 +37,16 @@ private:
     {
         std::string msg = buf->retrieveAllAsString();
         conn->send(msg);
-        conn->shutdown();
+        // conn->shutdown();
     }
-    EventLoop *loop_;
+    EventLoop loop_;
     TcpServer server_;
 };
 
 int main()
 {
-    EventLoop baseLoop;
     InetAddress addr(8000);
-    EchoServer server(&baseLoop, addr, "EchoServer-01"); // Acceptor non-blokcing listen create bind
-    server.start();  // (epoll_create、epoll_ctl)listen loopthread listenfd _> acceptChannel
-    baseLoop.loop(); // (epoll_wait循环)启动mainLoop底层的Poller
+    EchoServer server(addr, "EchoServer-01"); // Acceptor non-blokcing listen create bind
+    server.start();                           // (epoll_create、epoll_ctl)listen loopthread listenfd _> acceptChannel
     return 0;
 }
